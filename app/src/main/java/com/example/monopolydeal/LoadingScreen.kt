325f0201267f728460cd98dd738ab7843d0499d4
Rishi <rishijivani@hotmail.com>
2023-12-25 23:25:58 +0400

    HELLO

--- app/src/main/java/com/example/monopolydeal/LoadingScreen.kt
+++ app/src/main/java/com/example/monopolydeal/LoadingScreen.kt
@@ -1,4 +1,3 @@
-// LoadingScreen.kt
 package com.example.monopolydeal
 
 import android.content.Intent
@@ -6,18 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 
 class LoadingScreen : AppCompatActivity() {
 
     private val loadingDuration: Long = 2000
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
-
-        auth = FirebaseAuth.getInstance()
 
         Handler(Looper.getMainLooper()).postDelayed({
             checkUserAuthenticationAndNavigate()
@@ -25,14 +20,13 @@ class LoadingScreen : AppCompatActivity() {
     }
 
     private fun checkUserAuthenticationAndNavigate() {
-        val currentUser = auth.currentUser
+        // Replace this with your actual authentication check logic
+        val userLoggedIn = true
 
-        if (currentUser != null) {
-            // User is logged in, navigate to MainActivity
+        if (userLoggedIn) {
             val intent = Intent(this@LoadingScreen, MainActivity::class.java)
             startActivity(intent)
         } else {
-            // User is not logged in, navigate to MainMenu
             val intent = Intent(this@LoadingScreen, MainMenu::class.java)
             startActivity(intent)
         }
