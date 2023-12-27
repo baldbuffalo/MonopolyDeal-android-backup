d0c8cee5271996b6a8db5acefa3801f36b71eba3
Rishi <rishijivani@hotmail.com>
2023-12-27 23:57:37 +0400

    HELLO

--- app/src/main/java/com/example/monopolydeal/LoadingScreen.kt
+++ app/src/main/java/com/example/monopolydeal/LoadingScreen.kt
@@ -1,5 +1,6 @@
 package com.example.monopolydeal
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -59,43 +60,101 @@ class LoadingScreen : AppCompatActivity() {
                 } else {
                     // Finish the loading screen when the progress reaches the maximum value
                     progressBar.visibility = View.GONE
-                    checkUserAndNavigate()
+                    checkForUpdates()
+                    // Don't navigate here, we want to keep showing the progress bar
                 }
             }
         }, loadingDuration / (progressBar.max / 5))
     }
 
-    private fun checkUserAndNavigate() {
-        val currentUser: FirebaseUser? = auth.currentUser
-        if (currentUser == null) {
-            navigateToMainMenu()
-        } else {
-            navigateToMainActivity()
-        }
-    }
-
-    private fun navigateToMainMenu() {
-        val intent = Intent(this, MainMenu::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    private fun navigateToMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
     private fun checkForUpdates() {
-        // Implement your update checking logic here
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(githubRepoUrl)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle failure
+                runOnUiThread {
+                    Toast.makeText(this@LoadingScreen, "Failed to check for updates", Toast.LENGTH_SHORT).show()
+                    // After showing the error, continue with the loading screen
+                    showCheckingForUpdates()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.let {
+                    try {
+                        val jsonData = it.string()
+                        val json = JSONObject(jsonData)
+
+                        // Check if the "tag_name" key exists in the JSON response
+                        if (json.has("tag_name")) {
+                            val tagName = json.getString("tag_name")
+                            val latestVersion = tagName.substring(1) // Assuming tag_name is like "v1.2.3"
+
+                            // Compare with the current version installed on the device
+                            if (isNewerVersionAvailable(latestVersion)) {
+                                // Prompt the user to download the update
+                                runOnUiThread {
+                                    showUpdatePrompt()
+                                    // Keep showing the progress bar after the prompt
+                                    showCheckingForUpdates()
+                                }
+                            } else {
+                                // No update available, continue with the loading screen
+                                runOnUiThread {
+                                    showCheckingForUpdates()
+                                }
+                            }
+                        } else {
+                            // Handle the case where "tag_name" key is not present in the JSON response
+                            runOnUiThread {
+                                Toast.makeText(this@LoadingScreen, "Invalid JSON response", Toast.LENGTH_SHORT).show()
+                                showCheckingForUpdates()
+                            }
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                        runOnUiThread {
+                            Toast.makeText(this@LoadingScreen, "Error parsing JSON response", Toast.LENGTH_SHORT).show()
+                            showCheckingForUpdates()
+                        }
+                    }
+                }
+            }
+        })
     }
 
     private fun isNewerVersionAvailable(latestVersion: String): Boolean {
-        // Implement your version comparison logic here
-        return false
+        // Get the version code of the installed app
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
+        val installedVersionCode = packageInfo.versionCode
+
+        // Convert the latest version string to an integer
+        val latestVersionCode = latestVersion.toIntOrNull()
+
+        // Compare version codes
+        return latestVersionCode != null && latestVersionCode > installedVersionCode
     }
 
     private fun showUpdatePrompt() {
-        // Implement your update prompt logic here
+        // Show a dialog prompting the user to download the update
+        AlertDialog.Builder(this)
+            .setTitle("New Version Available")
+            .setMessage("A new version of the app is available. Do you want to download it?")
+            .setPositiveButton("Download") { _, _ ->
+                // Implement the logic to download the update (e.g., open a browser with the download link)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showCheckingForUpdates() {
+        // Set the text to "Checking for Updates" and continue showing the progress bar
+        val progressText = findViewById<TextView>(R.id.progressText)
+        progressText.text = getString(R.string.checking_for_updates)
     }
 }
